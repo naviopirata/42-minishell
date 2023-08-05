d017f04c188a6c818106b2646cacbec8a6d48142
Diego Diniz <ddiniz@student.42sp.org.br>
2023-08-05 16:41:03 -0400

    Feat: add mock parser function

--- src/minishell.c
+++ src/minishell.c
@@ -1,14 +1,28 @@
 #include "../incl/minishell.h"
 
+int	str_parse(char *lit)
+{
+	char **trim;
+	trim = ft_split(lit, ' ');
+	ft_printf("\n1: %s\n", trim[0]);
+	ft_printf("2: %s\n", trim[1]);
+	return 0;
+}
+
 int	main(void)
 {
-	// char *str;
-	signals_init();
-	abstree_init();
-	// while (1)
-	// {
-	// 	str = readline("enter ");
-	// 	ft_printf("minishell$ %s", str);
-	// }
+	char *str;
+	char *lit;
+
+	lit = "echo hi";
+	// signals_init();
+	// abstree_init();
+	while (1)
+	{
+		str = readline("\nenter ");
+		ft_printf("minishell$ %s", str);
+		if (ft_strnstr(str, lit, 7))
+			str_parse(lit);
+	}
 	return (0);
 }
