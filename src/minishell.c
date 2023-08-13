e37227d897a232505b8046cf3009532578fea448
Diego Diniz <ddiniz@student.42sp.org.br>
2023-08-13 18:42:07 -0400

    Feat: add execve v0 function

--- src/minishell.c
+++ src/minishell.c
@@ -1,4 +1,6 @@
 #include "../incl/minishell.h"
+#include <unistd.h>
+#include <sys/wait.h>
 
 int	str_parse(char *lit)
 {
@@ -9,6 +11,25 @@ int	str_parse(char *lit)
 	return 0;
 }
 
+void	cmd_run(void)
+{
+	int pid;
+	char *arr[] = {"cat", "echoed.txt", NULL};
+
+	pid = fork();
+	if (pid == 0)
+	{
+		ft_printf("\nChild pid: %d\n", pid);
+		execve("/bin/cat", arr , NULL);
+	}
+	else
+	{
+		wait(NULL);
+		ft_printf("Parent scope!...\n", pid);
+	}
+	return ;
+}
+
 int	main(void)
 {
 	char *str;
@@ -17,12 +38,14 @@ int	main(void)
 	lit = "echo hi";
 	// signals_init();
 	// abstree_init();
-	while (1)
-	{
-		str = readline("\nenter ");
+	// while (1)
+	// {
+		ft_printf("Enter your command:");
+		str = readline("");
 		ft_printf("minishell$ %s", str);
 		if (ft_strnstr(str, lit, 7))
 			str_parse(lit);
-	}
+		cmd_run();
+	// }
 	return (0);
 }
