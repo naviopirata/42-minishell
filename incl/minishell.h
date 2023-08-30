57b116df2e242179756fe19b42865a26d7c91497
bede-car <bede-car@student.42sp.org.br>
2023-08-29 21:31:52 -0300

    refact: include local libraries .h

--- incl/minishell.h
+++ incl/minishell.h
@@ -6,17 +6,21 @@
 /*   By: bede-car <bede-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 20:58:57 by ddiniz            #+#    #+#             */
-/*   Updated: 2023/08/24 20:16:20 by bede-car         ###   ########.fr       */
+/*   Updated: 2023/08/29 21:31:21 by bede-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+// allowed libraries
 # include "../lib/libft.h"
 # include <readline/readline.h>
 # include <signal.h>
 # include <string.h>
 
+// local libraries
+# include "./minishell_struct.h"
+# include "./minishell_prototypes.h"
 
 #endif
